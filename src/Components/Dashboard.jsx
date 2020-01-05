c9566c0dc273066338974cc2874955bc75420d04
Veronica Eulenberg <veronica.eulenberg@gmail.com>
2020-01-04 20:47:18 -0700

    dashboard has saved pet ids

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -1,12 +1,23 @@
 class Dashboard extends React.Component {
   constructor (props) {
     super(props)
+
+    let myPets = window.localStorage.getItem('myPets')
+    if (myPets !== null) {
+      myPets = JSON.parse(myPets)
+      this.state = {
+        myPets: myPets
+      }
+    }
   }
 
   render () {
     return (
       <div className='dashboard'>
         <h1>Dashboard</h1>
+        {this.state.myPets.map((petId) => {
+          return <div key={petId}>{petId}</div>
+        })}
       </div>
     )
   }
